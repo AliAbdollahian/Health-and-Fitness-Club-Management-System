673374b3fbdc3f002aee4d7394d6b871e2a667ac
AliAbdollahian <ALIABDOLLAHIAN@cmail.carleton.ca>
2024-04-09 16:21:31 -0400

    controller improved
    Added to controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.util.Date;
 
 public class Controller {
-    private JDateChooser dateChooser;
     public JdbcDatabaseOperations jdbcDatabaseOp;
     public GUI gui;
 
@@ -23,6 +22,27 @@ public class Controller {
     public void trainerClicked() {
         int idOfTrainer = Integer.parseInt(JOptionPane.showInputDialog("Enter your id "));
         Trainer trainer = jdbcDatabaseOp.getTrainerById(idOfTrainer);
+
+        if (trainer != null) {
+            JOptionPane.showMessageDialog(null, "Welcome, " + trainer.getFullName() + "!");
+            String[] options = {"Set your availability", "Search members by name"};
+            int choice = JOptionPane.showOptionDialog(null, "What would you like to do?", "Trainer Options", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+            switch (choice) {
+                case 0:
+                    setAvailability(trainer);
+                    break;
+                case 1:
+                    searchMembersByName();
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Trainer not found!");
+        }
+    }
+    private void setAvailability(Trainer trainer) {
         System.out.println("Current availability: " + trainer.getAvailability());
 
         JDateChooser dateChooser = new JDateChooser();
@@ -34,6 +54,17 @@ public class Controller {
             System.out.println("Availability updated successfully.");
         } else {
             System.out.println("Operation canceled.");
+        }
+    }
+
+    private void searchMembersByName() {
+        String memberName = JOptionPane.showInputDialog("Enter the name of the member: ");
+        Member member = jdbcDatabaseOp.getMemberByName(memberName);
+
+        if (member != null) {
+            JOptionPane.showMessageDialog(null, "Member found:\n" + member);
+        } else {
+            JOptionPane.showMessageDialog(null, "Member not found!");
         }
     }
 }
